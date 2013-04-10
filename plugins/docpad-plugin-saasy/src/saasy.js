292a15323b9190fa397a83c752d8b21d4b6b5d4e
neilbaylorrulez <neil.t@myplanetdigital.com>
2013-04-10 18:27:30 -0400

    Improved form creation

--- plugins/docpad-plugin-saasy/src/saasy.js
+++ plugins/docpad-plugin-saasy/src/saasy.js
@@ -1,38 +1,70 @@
-/*globals window, alert*/
+/*globals window, alert, console*/
 /*jslint plusplus: true*/
-var $S = (function (types) {
+var $S = {};
+
+$S = (function ($, types, undefined) {
     'use strict';
-    var id = 0;
-    function loadData(fileName) {
+     
+    if (typeof $ === 'undefined') {
+        throw "You can't use Saasy without jQuery";
+    }
+    
+    var $formSlot = $('.saasy .form-holder');
+
+    function buildForm(type, fileName) {
+        var key,
+            obj,
+            innerKey,
+            myId,
+            html = '';
+        function buildInput(type, id) {
+            return '<input id="' + id + '" type="' + type + '">';
+        }
+        function loadData() {
+            $.ajax({
+                url: '/saasy/document/' + fileName
+            }).done(function (obj) {
+                var key;
+                obj.meta.Content = obj.content;
+                for (key in obj.meta) {
+                    if (obj.meta.hasOwnProperty(key)) {
+                        $('#Saasy-' + key).attr('value', obj.meta[key]);
+                    }
+                }
+            });
+        }
+
+        type.fields.splice(0, 0, {Content: 'textarea'});
+        for (key in type.fields) {
+            if (type.fields.hasOwnProperty(key)) {
+                obj = type.fields[key];
+                innerKey = Object.keys(obj)[0];
+                myId = 'Saasy-' + innerKey;
+                html += '<label for="' + myId + '">' + innerKey + '</label>' + buildInput(type.fields[key][innerKey], myId);
+            }
+        }
+
+        if (fileName) {
+            loadData();
+        }
+        return html;
     }
 
     return {
-        buildForm: function (type, fileName) {
-            var key,
-                myId,
-                obj,
-                innerKey,
-                html = '';
-            function buildInput(type, id) {
-                return '<input id="' + id + '" type="' + type + '">';
-            }
-            type.fields.splice(0, 0, {Content: 'textarea'});
-            for (key in type.fields) {
-                if (type.fields.hasOwnProperty(key)) {
-                    myId = 'id_' + id++;
-                    obj = type.fields[key];
-                    innerKey = Object.keys(obj)[0];
-                    html += '<label for="' +  myId + '">' + innerKey + '</label>' + buildInput(type.fields[key][innerKey], myId);
-                }
-            }
-
-            if (fileName) {
-                loadData(fileName);
-            }
-            return html;
+        create: function (type) {
+            $formSlot.html(buildForm(type));
+        },
+        edit: function (type, fileName) {
+            $formSlot.html(buildForm(type, fileName));
+        },
+        delete: function () {
+        },
+        rename: function () {
+        },
+        curate: function () {
         }
     };
-}(window.contentTypes || {}));
+}(window.$, window.contentTypes || {}));
 
 
 /*
