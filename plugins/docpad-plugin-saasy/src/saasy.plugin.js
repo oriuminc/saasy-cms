cade72544df6fb24937ea38c7be4851b6c90a348
neilbaylorrulez <neil.t@myplanetdigital.com>
2013-04-10 15:55:17 -0400

    Adding content types and other stuff to saasy

--- plugins/docpad-plugin-saasy/src/saasy.plugin.js
+++ plugins/docpad-plugin-saasy/src/saasy.plugin.js
@@ -2,21 +2,21 @@
 var __hasProp = {}.hasOwnProperty,
   __extends = function(child, parent) {
     for (var key in parent) { 
-      if (__hasProp.call(parent, key)) child[key] = parent[key];
-    } 
-    function ctor() { 
-      this.constructor = child;
-    }
-    ctor.prototype = parent.prototype;
-    child.prototype = new ctor();
-    child.__super__ = parent.prototype;
-    return child;
+            if (__hasProp.call(parent, key)) child[key] = parent[key];
+        } 
+        function ctor() { 
+            this.constructor = child;
+        }
+        ctor.prototype = parent.prototype;
+        child.prototype = new ctor();
+        child.__super__ = parent.prototype;
+        return child;
   };
 
 // Pure JS module for DOCPAD
 module.exports = function(BasePlugin) {
   var Saasy,
-      jsFileContents,
+      saasyFileContents,
       fs = require('fs'),
       config;
 
@@ -28,46 +28,50 @@ module.exports = function(BasePlugin) {
       return Saasy.__super__.constructor.apply(this, arguments);
     }
 
+    function getContentTypes() {
+        var configPath = config.rootPath + '/saasy.config.json';
+        if (fs.existsSync(config.rootPath)) {
+            return JSON.parse(fs.readFileSync(configPath));
+        } else {
+            console.log("You have no content types defined in saasy.config.json");
+        }
+    }
+
     // Name our plugin
     Saasy.prototype.name = 'saasy';
 
     // Access our docpad configuration from within our plugin
     Saasy.prototype.docpadReady = function(opts) {
       config = opts.docpad.config;
+      config.contentTypes = getContentTypes();
     }
 
     // Inject our CMS front end to the server 'out' files
     Saasy.prototype.renderDocument = function(opts, next) {
       var extension = opts.extension, 
-          file = opts.file, 
-          index = opts.content.indexOf('</body>'),
-          injectJs = function () {
-            // If we have a </body> tag present
-            if(index > -1) {
-              // Add the jsFileContents before the </body> tag
-              opts.content = opts.content.replace('</body>',  jsFileContents + '</body>');
-            } else {
-              // Just inject the jsFileContents to the end of the layout
-              opts.content += jsFileContents;
-            }
-            return next(); // Move on to the next file 
-          };
+          file = opts.file;
+
+      function injectJs() {
+         opts.content += saasyFileContents;//opts.content.replace('</body>',  saasyFileContents + '</body>');
+         return next(); // Move on to the next file 
+      }
       
       // Only inject our CMS javascript into Layouts
       if (file.type === 'document' && file.attributes.isLayout) {
         // If we've previously read our saasy cms file then just inject the contents right away
-        if (jsFileContents) {
+        if (saasyFileContents) {
           injectJs();
         }
-
         // Read the contents of our Saasy CMS javascript file
-        return fs.readFile(__dirname + '/saasy.js', function (err, data) {
+        fs.readFile(__dirname + '/saasy.js', function (err, data) {
           if (err) {
             next();
             return console.log(err);
           }
+          var cssData = fs.readFileSync(__dirname + '/saasy.css'),
+              markup = fs.readFileSync(__dirname + '/saasy.html');
           // Build our JS file contents and inject them into the page markup
-          jsFileContents = '<script data-owner="saasy">' + data + '</script>';
+          saasyFileContents = '<style data-owner="saasy" type="text/css">' + cssData + '</style>' + markup + '<script data-owner="saasy">var contentTypes=' + JSON.stringify(config.contentTypes) + ';\n' + data + '</script>';
           injectJs();
         });
       } 
@@ -97,11 +101,14 @@ module.exports = function(BasePlugin) {
 
       // Write the contents of a file to DOCPATH documents folder
       function fileWriter(str, req) {
+        var format;
         if (req.body.type && req.body.url) {
           if (!fs.existsSync(config.documentsPaths + '/' + req.body.type)) {
             fs.mkdirSync(config.documentsPaths + '/' + req.body.type);
           }
-          fs.writeFileSync(config.documentsPaths + '/' + req.body.type + '/' + req.body.url + '.html.md', str);
+          format = req.body.format || 'html';
+
+          fs.writeFileSync(config.documentsPaths + '/' + req.body.type + '/' + req.body.url + '.' + format +'.md', str);
           return true;
         }
         return false;
@@ -136,11 +143,16 @@ module.exports = function(BasePlugin) {
       // Express REST like CRUD operations
 
       // Save a file
-      server.post('/saasy', function (req, res) {
-        if(fileWriter(fileBuilder(req), req)) {
+      function save() {
+         if(fileWriter(fileBuilder(req), req)) {
           return res.send(success);
         }
         return res.send(fail);
+      
+      }
+
+      server.post('/saasy', function (req, res) {
+        save();
       });
 
       // Delete a file
@@ -153,7 +165,7 @@ module.exports = function(BasePlugin) {
       
       // Edit a file
       server.post('/saasy/edit', function (req, res) {
-        res.send('API is running');
+        save(); 
       });
       
       // Rename a file
