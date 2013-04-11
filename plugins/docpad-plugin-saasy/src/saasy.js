7b51483a801e595ae4c2689e3e329d294702fcb1
Arvin Singla <arvin.singla@gmail.com>
2013-04-11 18:40:33 -0400

    Dynamic form builder can now save via the UI. SC-5

--- plugins/docpad-plugin-saasy/src/saasy.js
+++ plugins/docpad-plugin-saasy/src/saasy.js
@@ -1,28 +1,48 @@
 /*globals $S, window, alert, console*/
 /*jslint plusplus: true*/
 
-$S.API = (function ($, types) {
+$S.API = (function ($) {
     'use strict';
     if (typeof $ === 'undefined') {
         throw 'You can\'t use Saasy without jQuery';
     }
 
-    var $formSlot = $('#saasy .form-holder');
+    var $menuSlot = $('#saasy .menu-holder');
+    var $formSlot = $('#saasy #ct-form');
+
+    function init () {
+        var key,
+            html = '';
+        for (key in $S.contentTypes) {
+            if ($S.contentTypes.hasOwnProperty(key)) {
+                html += '<a href=\'javascript:$S.API.createForm($S.contentTypes[' + key + '])\'>' + $S.contentTypes[key].name + '</a>';
+            }
+        }
+        $menuSlot.html(html);
+
+
+        $formSlot.on( "submit", function( event ) {
+          event.preventDefault();
+          $S.API.create($(this).serialize());
+        });
+
+    }
 
     function buildForm(type, fileName) {
         var key,
             obj,
             innerKey,
             myId,
+            myName,
             html = '';
-        function buildInput(type, id) {
+        function buildInput(type, id, name) {
             switch (type) {
             case 'textarea':
-                return '<textarea id="' + id + '"></textarea>';
+                return '<textarea id="' + id + '" name="' + name + '"></textarea>';
             case 'image':
-                return '<input id="' + id + '" type="file">';
+                return '<input id="' + id + '" name="' + name + '" type="file">';
             default:
-                return '<input id="' + id + '" type="' + type + '">';
+                return '<input id="' + id + '" name="' + name + '" type="' + type + '">';
             }
         }
 
@@ -31,7 +51,11 @@ $S.API = (function ($, types) {
                 url: '/saasy/document/' + fileName
             }).done(function (result) {
                 var key;
-                result.meta.Content = result.content;
+
+                // TODO: Eventually this data needs to be read from a merge of the global fields
+                // and the content type fields as they are the canonical source. 
+                result.meta.content = result.content;
+                
                 for (key in result.meta) {
                     if (result.meta.hasOwnProperty(key)) {
                         $('#saasy-form-' + key).attr('value', result.meta[key]);
@@ -40,9 +64,12 @@ $S.API = (function ($, types) {
             });
         }
 
-        //Add Content implicitely to all datatypes
-        if (type.fields.length && Object.keys(type.fields[0])[0] !== ('Content')) {
-            type.fields.splice(0, 0, {Content: 'textarea'});
+        for (key in $S.globalFields) {
+            if ($S.globalFields.hasOwnProperty(key)) {
+                obj = $S.globalFields[key];
+                myId = 'saasy-form-' + key;
+                html += '<label for="' + myId + '">' + key + '</label>' + buildInput($S.globalFields[key], myId, key);
+            }
         }
 
         for (key in type.fields) {
@@ -50,23 +77,37 @@ $S.API = (function ($, types) {
                 obj = type.fields[key];
                 innerKey = Object.keys(obj)[0];
                 myId = 'saasy-form-' + innerKey;
-                html += '<label for="' + myId + '">' + innerKey + '</label>' + buildInput(type.fields[key][innerKey], myId);
+                html += '<label for="' + myId + '">' + innerKey + '</label>' + buildInput(type.fields[key][innerKey], myId, innerKey);
             }
         }
 
         if (fileName) {
             loadData();
         }
+        html += '<label for="saasy-form-layout">Layout</label><input id="saasy-form-layout" type="text" name="layout" value="' + type.layout + '">';
+        html += '<input type="hidden" name="type" value="' + type.type + '">';
+        html += '<input type="submit" value="Submit">';
         
         return html;
     }
 
+    init();
+
     return {
-        create: function (type) {
+        createForm: function (type) {
             $formSlot.html(buildForm(type));
         },
-        edit: function (type, fileName) {
+        editForm: function (type, fileName) {
             $formSlot.html(buildForm(type, fileName));
+        },
+        create: function(data) {
+            $.ajax({
+                url: '/saasy/',
+                type: 'POST',
+                data: data
+            }).done(function (result) {
+                console.log(result);
+            });
         },
         delete: function () {
         },
@@ -75,4 +116,4 @@ $S.API = (function ($, types) {
         curate: function () {
         }
     };
-}(window.$, window.contentTypes || {}));
+}(window.$));
